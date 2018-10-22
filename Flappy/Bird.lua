9f72b4f3a72646e11bc4b0dcb196a697d4abd09b
Vultys750 <fan.of.Dnipro@gmail.com>
2018-10-22 12:43:51 +0300

    Flappy-8
    The State Machine Update

--- Flappy/Bird.lua
+++ Flappy/Bird.lua
@@ -17,9 +17,9 @@ function Bird:collides(pipe)
     if (self.x + 2) + (self.width - 4) >= pipe.x and self.x + 2 <= pipe.x + PIPE_WIDTH then
         if (self.y + 2) + (self.height - 4) >= pipe.y and self.y + 2 <= pipe.y + PIPE_HEIGHT then
             return true
-        end
+        end     
     end
-    
+
     return false
 end
 
