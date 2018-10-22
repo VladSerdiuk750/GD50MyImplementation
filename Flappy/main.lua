3f83258c3ce5352e159bc44e087b113fa2b8cb20
Vultys750 <fan.of.Dnipro@gmail.com>
2018-10-22 15:49:45 +0300

    Flappy-10
    The Countdown Update

--- Flappy/main.lua
+++ Flappy/main.lua
@@ -10,6 +10,7 @@ require 'PipePair'
 
 require 'StateMachine'
 require 'states/BaseState'
+require 'states/CountdownState'
 require 'states/PlayState'
 require 'states/ScoreState'
 require 'states/TitleScreenState'
@@ -54,6 +55,7 @@ function love.load()
   
     gStateMachine = StateMachine {
         ['title'] = function() return TitleScreenState() end,
+        ['countdown'] = function() return CountdownState() end,
         ['play'] = function() return PlayState() end,
         ['score'] = function() return ScoreState() end
     }
