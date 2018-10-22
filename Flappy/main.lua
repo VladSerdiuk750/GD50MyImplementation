855dafb4680cde928051dba748fdad34043b0e2e
Vultys750 <fan.of.Dnipro@gmail.com>
2018-10-22 13:02:02 +0300

    Flappy-9
    The Score Update

--- Flappy/main.lua
+++ Flappy/main.lua
@@ -11,6 +11,7 @@ require 'PipePair'
 require 'StateMachine'
 require 'states/BaseState'
 require 'states/PlayState'
+require 'states/ScoreState'
 require 'states/TitleScreenState'
 
 WINDOW_WIDTH = 1280
@@ -54,6 +55,7 @@ function love.load()
     gStateMachine = StateMachine {
         ['title'] = function() return TitleScreenState() end,
         ['play'] = function() return PlayState() end,
+        ['score'] = function() return ScoreState() end
     }
     gStateMachine:change('title')
 
