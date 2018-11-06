036d6377bf5816ffc222c3c472352d4dc1dba387
Vultys750 <fan.of.Dnipro@gmail.com>
2018-11-06 16:44:57 +0200

    Second task
    giving medal for perfomance

--- Bird/assignment1/states/ScoreState.lua
+++ Bird/assignment1/states/ScoreState.lua
@@ -10,6 +10,11 @@
 
 ScoreState = Class{__includes = BaseState}
 
+local gold = love.graphics.newImage('gold.png')
+local silver = love.graphics.newImage('silver.png')
+local bronze = love.graphics.newImage('bronze.png')
+local wood = love.graphics.newImage('wood.png')
+
 --[[
     When we enter the score state, we expect to receive the score
     from the play state so we know what to render to the State.
@@ -32,6 +37,21 @@ function ScoreState:render()
 
     love.graphics.setFont(mediumFont)
     love.graphics.printf('Score: ' .. tostring(self.score), 0, 100, VIRTUAL_WIDTH, 'center')
+
+
+    if self.score == 0 then 
+        love.graphics.draw(wood, VIRTUAL_WIDTH / 2 - ((wood:getWidth()*0.1/2)), 0, 0, 0.1)
+    elseif self.score > 0 then
+        if self.score > 9 then
+            if self.score > 14 then 
+                love.graphics.draw(gold, VIRTUAL_WIDTH / 2 - ((gold:getWidth()*0.1/2)), 0, 0, 0.1)
+            else 
+                love.graphics.draw(silver, VIRTUAL_WIDTH / 2 - ((silver:getWidth()*0.15/2)), 0, 0, 0.15)
+            end
+        else
+            love.graphics.draw(bronze, VIRTUAL_WIDTH / 2 - ((bronze:getWidth()*0.25/2)), 0, 0, 0.25)
+        end
+    end
 
     love.graphics.printf('Press Enter to Play Again!', 0, 160, VIRTUAL_WIDTH, 'center')
 end
